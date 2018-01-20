af057289a2129ce5dae4ae5cfa27a4039b277960
Vay3t <fffyanez@gmail.com>
2018-01-19 21:47:42 -0300

    Update module.php

--- openvpn/api/module.php
+++ openvpn/api/module.php
@@ -9,17 +9,10 @@ class openvpn extends Module
     public function route()
     {
         switch ($this->request->action) {
-            case 'getContents':    // If you request the action "getContents" from your Javascript, this is where the PHP will see it, and use the correct function
-            $this->getContents();  // $this->getContents(); refers to your private function that contains all of the code for your request.
+            case 'refreshInfo':    // If you request the action "getContents" from your Javascript, this is where the PHP will see it, and use the correct function
+            $this->refreshInfo();  // $this->getContents(); refers to your private function that contains all of the code for your request.
             break;                 // Break here, and add more cases after that for different requests.
         }
-    }
-
-    private function getContents()  // This is the function that will be executed when you send the request "getContents".
-    {
-        $this->response = array("success" => true,    // define $this->response. We can use an array to set multiple responses.
-                                "greeting" => "Hey there!",
-                                "content" => "This is the HTML template for your new module! The example shows you the basics of using HTML, AngularJS and PHP to seamlessly pass information to and from Javascript and PHP and output it to HTML.");
     }
     
     protected function refreshInfo()
